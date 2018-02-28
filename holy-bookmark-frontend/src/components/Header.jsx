f72938aa4ef5e610c68ed0892bc3e29b0d23d076
Nikolay Stupak <nikolaystupak@gmail.com>
2018-02-28 17:32:20 +0300

    frontend: The logo in the header is made a link to the root page

--- holy-bookmark-frontend/src/components/Header.jsx
+++ holy-bookmark-frontend/src/components/Header.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
-import theme from '../theme/index';
+import theme from '../theme';
+import { Link } from '../components/Link';
 
 const StyledHeader = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 2rem 0;
-  background: ${theme.color.primary};
-  color: ${theme.color.primaryInvert};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem 0;
+    background: ${theme.color.primary};
+    color: ${theme.color.primaryInvert};
 `;
 
 const Logo = styled.h1`
-  font-size: ${theme.title.primary.font.size};
+    font-size: ${theme.title.primary.font.size};
+  
+    ${Link} {
+        color: ${theme.color.primaryInvert};
+    
+        &:hover,
+        &:focus,
+        &:active,
+        &:visited {
+            color: ${theme.color.primaryInvert};
+        }
+    }
 `;
 
 const Motto = styled.p`
-  margin-top: 2rem;
-  font-size: ${theme.title.secondary.font.size};
+    margin-top: 2rem;
+    font-size: ${theme.title.secondary.font.size};
 `;
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo>Holy Bookmark</Logo>
+            <Logo><Link to="/">Holy Bookmark</Link></Logo>
             <Motto>There should be a beautiful motto but it's not there</Motto>
         </StyledHeader>
     );
